61a845e153374707d8575dc2614324f89126c8f7
Raphaël Fromentin <rafbodaha@gmail.com>
2024-11-05 15:02:22 +0100

    feat: Add authentication controller

--- src/main/java/fr/cyu/jee/controller/AuthController.java
+++ src/main/java/fr/cyu/jee/controller/AuthController.java
@@ -1,11 +1,25 @@
 package fr.cyu.jee.controller;
 
+import fr.cyu.jee.dto.LoginDTO;
+import fr.cyu.jee.dto.RegisterDTO;
+import fr.cyu.jee.model.User;
+import fr.cyu.jee.model.UserType;
+import fr.cyu.jee.service.AuthService;
+import fr.cyu.jee.service.UserRepository;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.util.Optional;
+
 @Controller
 public class AuthController {
+
+    @Autowired
+    private AuthService authService;
 
     @RequestMapping(value="/login", method = RequestMethod.GET)
     public String getLoginPage(){
