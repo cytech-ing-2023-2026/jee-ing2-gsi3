e9899b2cf6e3450f1c878a92cdf0d61c46bcfb3d
Raphaël Fromentin <rafbodaha@gmail.com>
2024-11-05 15:08:45 +0100

    feat: Add default admin account registration

--- src/main/java/fr/cyu/jee/CommandLineAppStartupRunner.java
+++ src/main/java/fr/cyu/jee/CommandLineAppStartupRunner.java
@@ -1,8 +1,8 @@
 package fr.cyu.jee;
 
-import fr.cyu.jee.model.Admin;
-import fr.cyu.jee.model.User;
-import fr.cyu.jee.service.UserRepository;
+import fr.cyu.jee.dto.RegisterDTO;
+import fr.cyu.jee.model.UserType;
+import fr.cyu.jee.service.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -11,16 +11,12 @@ import java.time.LocalDate;
 
 @Component
 public class CommandLineAppStartupRunner implements CommandLineRunner {
+
     @Autowired
-    UserRepository userRepository;
+    AuthService authService;
 
     @Override
     public void run(String...args) {
-        System.out.println("Registering admin...");
-        if(userRepository.findByEmail("notch@minecraft.net") == null) {
-            User admin = new Admin("notch@minecraft.net", "markus", "Markus", "Persson", LocalDate.of(1979, 6, 1));
-            userRepository.save(admin);
-            System.out.println("Admin hash: " + userRepository.findByEmail("notch@minecraft.net").getPassword());
-        }
+        authService.register(new RegisterDTO("Markus", "Persson", LocalDate.of(1979, 6, 1), "notch@minecraft.net", "markus"), UserType.ADMIN);
     }
 }
