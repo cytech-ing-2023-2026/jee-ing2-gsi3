620aa5005428eab6a03c29c6460219d72a40c23f
Raphaël Fromentin <rafbodaha@gmail.com>
2024-11-18 12:46:43 +0100

    refactor: Move login logic to middleware

---
+++ src/main/java/fr/cyu/jee/AppConfig.java
@@ -0,0 +1,17 @@
+package fr.cyu.jee;
+
+import fr.cyu.jee.middleware.AuthInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class AppConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new AuthInterceptor()).addPathPatterns(
+                "/home"
+        );
+    }
+}
