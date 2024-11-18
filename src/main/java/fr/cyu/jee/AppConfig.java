bee18eebabff4420b44a5b7a7c86df10aed22368
Raphaël Fromentin <rafbodaha@gmail.com>
2024-11-18 12:49:13 +0100

    feat: Add admin interceptor

--- src/main/java/fr/cyu/jee/AppConfig.java
+++ src/main/java/fr/cyu/jee/AppConfig.java
@@ -1,5 +1,6 @@
 package fr.cyu.jee;
 
+import fr.cyu.jee.middleware.AdminInterceptor;
 import fr.cyu.jee.middleware.AuthInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -10,8 +11,12 @@ public class AppConfig implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
+        //TODO Add other endpoints
         registry.addInterceptor(new AuthInterceptor()).addPathPatterns(
                 "/home"
         );
+
+        //TODO Add admin endpoints
+        //registry.addInterceptor(new AdminInterceptor());
     }
 }
