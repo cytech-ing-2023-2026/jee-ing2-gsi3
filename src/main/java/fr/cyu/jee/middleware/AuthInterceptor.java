a74c48d2db66878d192075860ac56b2a37576349
Raphaël Fromentin <rafbodaha@gmail.com>
2024-11-18 14:33:32 +0100

    feat: Add redirection info to auth middleware

--- src/main/java/fr/cyu/jee/middleware/AuthInterceptor.java
+++ src/main/java/fr/cyu/jee/middleware/AuthInterceptor.java
@@ -4,12 +4,14 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.web.servlet.HandlerInterceptor;
 
+import java.net.URI;
+
 public class AuthInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         if(request.getSession().getAttribute("user") == null) {
-            response.sendRedirect("/login");
+            response.sendRedirect(URI.create("/login?redirect=" + request.getRequestURI()).toASCIIString());
             return false;
         } else return true;
     }
